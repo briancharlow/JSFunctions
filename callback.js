644443b047f431610232b788b12d389cf1995fdb
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2023-06-30 10:57:59 +0300

    adds the callback function

--- callback.js
+++ callback.js
@@ -1,1 +1,15 @@
 // Write a JavaScript function that takes a callback and invokes it after a delay of 2 second.
+
+
+function callback(delayedFunction) {
+    setTimeout(delayedFunction, 5000);
+}
+
+function delayedFunction() {
+    console.log("After callback");
+}
+
+
+console.log("Before callback");
+callback(delayedFunction);
+
