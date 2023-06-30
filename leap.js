511c05ffa2486ec99e6ebdff3679abbd0e5f1266
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2023-06-30 10:40:03 +0300

    adds the leap years function

--- leap.js
+++ leap.js
@@ -1,1 +1,14 @@
 // Write a JavaScript program to find the leap years in a given range of years.
+
+function leapYears(start, end) {
+    let leapYears = [];
+
+    for (let year = start; year <= end; year++) {
+        if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+            leapYears.push(year);
+        }
+    }
+
+    return leapYears;
+}
+console.log(leapYears(2000, 2012));
