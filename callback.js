8fbb956e81a7e36790dc08c4fafb5b4c3e0c6657
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2023-06-30 10:37:43 +0300

    adds the intsum function

---
+++ callback.js
@@ -0,0 +1,1 @@
+// Write a JavaScript function that takes a callback and invokes it after a delay of 2 second.
---
+++ leap.js
@@ -0,0 +1,1 @@
+// Write a JavaScript program to find the leap years in a given range of years.
